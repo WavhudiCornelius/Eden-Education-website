1e19fa51f10c14948ee2e28e1a4f94b793d32e99
Cornelius Wavhudi Mulaudzi <corneliusmulaudzi@gmail.com>
2020-07-22 21:01:38 +0200

    Added comments to the main.js file

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+// select all the document input tags
 const inputs = document.querySelectorAll(".input");
 
-
+// add a focus class to the .input-div class
 function addcl(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
+// remove a focus class to the .input-div class
 function remcl(){
 	let parent = this.parentNode.parentNode;
 	if(this.value == ""){
@@ -13,7 +15,8 @@ function remcl(){
 	}
 }
 
-
+// adds an EventListener to all the inputs of the Document
+// to listen to when the input field is being focused on or not
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
